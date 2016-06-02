89f88b69c0c21f549367b8c502f9ae67e03dfccf
Dmytro Tolok <Dmytro_Tolok@epam.com>
2016-06-02 19:33:38 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  entry: './home',
+  output: {
+    filename: './build.js',
+    library: 'home'
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 400
+  }
+}
